2bdadc02c4cc15db87fd89daca68c735469af635
Kurt Dillen <kurt.dillen@dls-belgium.com>
2015-02-17 09:24:34 +0100

    * Adding service part for haproxy. [SYS-436], [SYS-437]

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
 # init file for HAProxy Installation
 #
 class haproxy (
+  $haproxy_service  = true,
 )
 {
 
   ### Declaring calculated variables
   anchor  { 'haproxy::start': }->
   class   { 'haproxy::packages': }->
+  class   { 'haproxy::service': }->
   anchor  { 'haproxy::end': }
 }
---
+++ manifests/service.pp
@@ -0,0 +1,26 @@
+# = Class: haproxy::service
+#
+# Create haproxy Services
+#
+class haproxy::service
+{
+
+  if $haproxy::haproxy_service
+  {
+    #### Enable the services
+    service { 'haproxy':
+      ensure  => running,
+      enable  => true,
+      require => Package[$haproxy::packages::haproxy_packages],
+    }
+  }
+  else
+  {
+    #### Disable the services
+    service { 'haproxy':
+      ensure  => stopped,
+      enable  => false,
+      require => Package[$haproxy::packages::haproxy_packages],
+    }
+  }
+}
